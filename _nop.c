e70e84ba6e8365ac1f87a68248c659b1f2b44841
nellynakhero <nelly.nakhero@gmail.com>
2022-06-16 21:29:01 +0300

    mull.c

---
+++ _nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn nothing.
+ *
+ * @stack:  double pointer to the head of the stack.
+ *
+ * @line_number:  Number of the line.
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
