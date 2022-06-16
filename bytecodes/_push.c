297bcaea314818458618c6b9b86c836c20378b45
nellynakhero <nelly.nakhero@gmail.com>
2022-06-16 10:49:35 -0700

    push pall

---
+++ bytecodes/_push.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * _push - push element to the stack
+ * @stack : doubly pointer to head of stack
+ * @line_num: number of the line
+ * Return: Nothing.
+ */
+void _push(stack_t **stack, unsigned int line_num)
+{
+	char *value = strtok(NULL, DELIMITERS);
+	stack_t *new_node;
+
+	if (!stack)
+	{
+		fprintf(stderr, "L%d: Need to use line_num\n", line_num);
+		exit(2);
+	}
+
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(value);
+	new_node->n = n;
+	new_node->next = (*stack);
+	new_node->prev = NULL;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
